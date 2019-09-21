c5722789a83781a1d04c46134941485b48ad21d6
Jamey-Dogom <33010705+AppJedi@users.noreply.github.com>
2019-09-21 11:01:36 -0700

    akshay2

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 let app = new express();
 
 app.use(express.json());
-app.use(express.static(__dirname + '/angular_app/public/dist/public'));
+app.use(express.static(__dirname + '/public/dist/public'));
 
 require('./server/config/mongoose');
 require('./server/config/routes')(app);
 
-app.all('*', (req, res) => res.sendFile(__dirname + '/angular_app/public/dist/public/index.html'));
+app.all('*', (req, res) => res.sendFile(__dirname + '/public/dist/public/index.html'));
 
 app.listen(3333, () => console.log('Listening on port 9000...'));
